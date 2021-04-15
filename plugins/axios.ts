24417a9318042584f3058c34c31f0c22d7e115d2
jiyaaany <jiyaaany@gmail.com>
2021-04-15 17:56:27 +0900

    feature/axios

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,11 +1,9 @@
-import _axios from 'axios';
+import _axios, { AxiosInstance } from 'axios';
 
-const axios = _axios.create({
+export const axios: AxiosInstance = _axios.create({
     baseURL: '//localhost:5000',
     proxy: {
         host: '//localhost',
         port: 5000
     }
 });
-
-export default axios;
